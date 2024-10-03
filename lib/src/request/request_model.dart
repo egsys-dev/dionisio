90dd2b76eeac5d9eaa05ece85b60847bd314e5b0
Guilherme Lemos <guilherme.lemos@egsys.com.br>
2024-10-03 14:43:54 -0300

    #BH-27 feat: adicionada possibilidade de informar o content-type na requisição

--- lib/src/request/request_model.dart
+++ lib/src/request/request_model.dart
@@ -6,6 +6,7 @@ class RequestModel {
   final dynamic params;
   final dynamic queryParameters;
   final bool tokenEnabled;
+  final String? contentType;
 
   RequestModel({
     required this.endpoint,
@@ -13,6 +14,7 @@ class RequestModel {
     this.params,
     this.queryParameters,
     this.tokenEnabled = true,
+    this.contentType,
   });
 
   RequestModel.post({
@@ -20,66 +22,78 @@ class RequestModel {
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.POST,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   RequestModel.get({
     required String endpoint,
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.GET,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   RequestModel.put({
     required String endpoint,
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.PUT,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   RequestModel.delete({
     required String endpoint,
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.DELETE,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   RequestModel.patch({
     required String endpoint,
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.PATCH,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   RequestModel.head({
     required String endpoint,
     dynamic params,
     dynamic queryParameters,
     bool tokenEnabled = true,
+    String? contentType,
   })  : endpoint = endpoint,
         method = Method.HEAD,
         params = params,
         queryParameters = queryParameters,
-        tokenEnabled = tokenEnabled;
+        tokenEnabled = tokenEnabled,
+        contentType = contentType;
 
   Map<String, dynamic> toMap() {
     return {
@@ -88,6 +102,7 @@ class RequestModel {
       'params': params,
       'queryParameters': queryParameters,
       'tokenEnabled': tokenEnabled,
+      'contentType': contentType,
     };
   }
 
@@ -98,6 +113,7 @@ class RequestModel {
       params: map['params'],
       queryParameters: map['queryParameters'],
       tokenEnabled: map['tokenEnabled'],
+      contentType: map['contentType'],
     );
   }
 }
