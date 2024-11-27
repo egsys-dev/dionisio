4cc4a9cf79928fec0198727c03fabc846fe669a4
lucasalmeida-egsys <77992211+lucasalmeida-egsys@users.noreply.github.com>
2024-11-27 09:45:26 -0300

    Update http_remote_impl.dart

--- lib/src/http/http_remote_impl.dart
+++ lib/src/http/http_remote_impl.dart
@@ -56,6 +56,7 @@ class HttpRemoteImpl implements HttpRemote {
       if (_isErroInesperado(error)) {
         if (kDebugMode) {
           log(error.message);
+          log(error.response?.data);
         }
 
         rethrow;
@@ -103,6 +104,7 @@ class HttpRemoteImpl implements HttpRemote {
       if (_isErroInesperado(error)) {
         if (kDebugMode) {
           log(error.message);
+          log(error.response?.data);
         }
 
         rethrow;
